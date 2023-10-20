fbbd62b58adaad27b5cddbb2471f5c8fe1d37cbf
MacAndKaj <mkajdak@gmail.com>
2023-10-20 20:39:05 +0200

    Script to create image

--- build_rel_image.sh
+++ build_rel_image.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-sudo docker build -f docker/rel/mh_rel_aarch64.dockerfile .
+# Exit on any error
+set -o errexit
+
+function usage ()
+{
+  echo "Usage: $0 [-i|--image <image filename>] [-t|--tag <docker image tag]"
+  echo "   -i|--image   Set filename for archived docker image."
+  echo "   -t|--tag     Set tag name for building docker image."
+  exit 0
+}
+
+#while getopts ""; do
+#
+#done
+
+
+DOCKER_TAG="motur_rel_image"
+
+git submodule update --remote --merge
+
+docker build -f docker/rel/mh_rel_aarch64.dockerfile -t "${DOCKER_TAG}" .
+
+docker save "${DOCKER_TAG}" | gzip > "${DOCKER_TAG}".tar.gz
